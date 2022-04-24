1fa86290ceb0d7535cf50b0fa88cab3dfd4fca1a
hx1098 <1098269542@qq.com>
2022-04-24 21:36:47 +0800

    初始化

---
+++ dongbao/src/main/java/com/hx/dongbao/DongbaoApplication.java
@@ -0,0 +1,16 @@
+package com.hx.dongbao;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class DongbaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DongbaoApplication.class, args);
+    }
+
+}
